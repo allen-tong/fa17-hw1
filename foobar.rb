1cac88a4e18bb81e912e11f77eb5b6493081a754
Allen Tong <allentong29@gmail.com>
2017-09-19 21:01:22 -0700

    Completed hw1

--- foobar.rb
+++ foobar.rb
@@ -1,9 +1,13 @@
 class Foobar
 
   def self.baz(a)
-    # Class method
-    # Call with `Foobar.baz`
+    a = a.map { |e| e.to_i + 2 }
+    a.keep_if { |e| e % 2 == 0 }
+    a.uniq!
+    a.reject! { |e| e > 10 }
+    sum = 0
+    a.each { |e| sum += e }
+    return sum
   end
+
 end
-
-
